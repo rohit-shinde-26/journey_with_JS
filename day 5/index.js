0e7f92b585ef1315a427e18badfbd9f7bcd9e6c8
Rohit <rohitshinde26072003@gmail.com>
2023-12-22 15:13:56 +0530

    array was done for today

---
+++ day 5/index.js
@@ -0,0 +1,14 @@
+let a ;
+a = 8;
+// console.log(a);
+
+// array 
+let arr = [565,87979,"hello",765,"world"];
+// console.log(arr);
+
+let cars = ["volvo","BMW"]
+let comp = cars.toString();
+// console.log(cars.length);
+console.log(typeof comp)
+console.log(typeof cars);
+
